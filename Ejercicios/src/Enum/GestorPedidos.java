dee7c6733b78644e2ba7c2d4f8cdcefae00891ca
JoseRafael <jose_rafael2@unitec.edu>
2026-08-12 07:53:18 -0600

    Clase de enum

---
+++ Ejercicios/src/Enum/GestorPedidos.java
@@ -0,0 +1,15 @@
+package Enum;
+
+public class GestorPedidos {
+    private final Pedido pedidos[];
+    private int cantidad;
+    
+    public GestorPedidos(int size){
+        pedidos = new Pedido[size];
+        cantidad = 0;
+    }
+
+    public void AgregarPedido(Pedido dato) throws CapacidadException{
+        
+    }
+}
